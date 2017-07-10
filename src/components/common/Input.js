8bbd91e960b1ab55c84d13b0d9bb1c816e099a51
jordans <schuringa.jordan@gmail.com>
2017-07-10 15:10:18 +1000

    Added proptypes for custom input component

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { TextInput, View, Text } from 'react-native'
 import PropTypes from 'prop-types'
 
-const Input = ({ label }) => {
+const Input = ({ label, value, onChangeText }) => {
   return (
     <View>
       <Text> {label} </Text>
+      <TextInput
+        style={{ height: 20, width: 100 }}
+        value={value}
+        onChangeText={onChangeText}
+      />
     </View>
   )
 }
 
 Input.propTypes = {
-  label: PropTypes.string
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChangeText: PropTypes.func
 }
 
 export { Input }
