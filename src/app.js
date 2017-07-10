de1da1abfda2237ef185bf48d4e0d5cfeaabba88
jordans <schuringa.jordan@gmail.com>
2017-07-10 14:40:18 +1000

    Exporting componenets from components/common/index.js. Added firebase support

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react'
+import firebase from 'firebase'
+import { View, Text } from 'react-native'
+import { Header } from './components/common'
 
 class App extends Component {
-  render() {
+  componenetWillMount () {
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAREqqoQA_wywN9PXRzmBsar0VSmFC1S2s',
+      authDomain: 'udemy-auth-c3c7c.firebaseapp.com',
+      databaseURL: 'https://udemy-auth-c3c7c.firebaseio.com',
+      projectId: 'udemy-auth-c3c7c',
+      storageBucket: 'udemy-auth-c3c7c.appspot.com',
+      messagingSenderId: '697175397000'
+    })
+  }
+
+  render () {
     return (
       <View>
+        <Header headerText='Authentication' />
         <Text> An App! </Text>
       </View>
-    );
+    )
   }
 }
 
-export default App;
+export default App
