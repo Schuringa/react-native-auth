0a360aea7f7c3c3bb7c9a642853aaa0d3df42762
jordans <schuringa.jordan@gmail.com>
2017-07-10 14:48:53 +1000

    Created LoginForm component.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import { Header } from './components/common'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
   componenetWillMount () {
@@ -20,7 +21,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText='Authentication' />
-        <Text> An App! </Text>
+        <LoginForm />
       </View>
     )
   }
